c4703f836d484f03ebeda8e38fbe3a17ee898fd5
elsie <elsie@elsiedeMacBook-Pro.local>
2020-11-04 16:17:00 +0800

    feat: setState & range

--- main.js
+++ main.js
@@ -1,9 +1,18 @@
 import { createElement, Component, render } from './toy-react.js'
 class MyComponent extends Component{
+    constructor() {
+        super();
+        this.state = {
+            a: 1,
+            b: 2
+        }
+    }
     render() {
         return <div>
             <h1>my component</h1>
-            {this.children}
+            <button onclick={() => this.setState({ a: this.state.a+1 })}>click</button>
+            <span>{this.state.a.toString()}</span>
+            <span>{this.state.b.toString()}</span>
         </div>
     }
 }
